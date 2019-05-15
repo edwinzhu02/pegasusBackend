2f4e24673c49295009994aa07dd32141da9885e7
jy202715 <jy37110@gmail.com>
2019-05-15 18:16:03 +1200

    Finished teacher filter controller

---
+++ Controllers/TeacherFilterController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pegasus_backend.Models;
+
+namespace Pegasus_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherFilterController : ControllerBase
+    {
+        private readonly pegasusContext.ablemusicContext _ablemusicContext;
+
+        public TeacherFilterController(pegasusContext.ablemusicContext ablemusicContext)
+        {
+            _ablemusicContext = ablemusicContext;
+        }
+
+        // GET: api/TeacherFilter
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            Result<List<Object>> result = new Result<List<Object>>();
+            dynamic orgs;
+            dynamic rooms;
+            dynamic levels;
+            dynamic teachers;
+
+            try
+            {
+                orgs = await (from o in _ablemusicContext.Org
+                              select new
+                              {
+                                  o.OrgId,
+                                  o.OrgName,
+                              }).ToListAsync();
+                rooms = await (from r in _ablemusicContext.Room
+                               select new
+                               {
+                                   r.RoomId,
+                                   r.RoomName,
+                                   r.OrgId
+                               }).ToListAsync();
+                levels = await (from l in _ablemusicContext.Lookup
+                                where l.LookupType == 1
+                                select new
+                                {
+                                    l.PropValue,
+                                    l.PropName,
+                                }).ToListAsync();
+                teachers = await (from t in _ablemusicContext.Teacher
+                                  join ta in _ablemusicContext.AvailableDays on t.TeacherId equals ta.TeacherId
+                                  select new
+                                  {
+                                      t.TeacherId,
+                                      TeacherFirstName = t.FirstName,
+                                      TeacherLastName = t.LastName,
+                                      TeacherAvailableOrgId = ta.OrgId,
+                                      TeacherLevelId = t.Level,
+                                  }).ToListAsync();
+            }
+            catch(Exception ex)
+            {
+                result.IsSuccess = false;
+                result.ErrorMessage = ex.Message;
+                return BadRequest(result);
+            }
+            
+            var teachersNoRepeat = teachers.Distinct().ToList();
+            List<Object> teacherResults = new List<Object>();
+            foreach(var org in orgs)
+            {
+                List<Object> levelList = new List<Object>();
+                foreach(var level in levels)
+                {
+                    List<Object> teacherList = new List<Object>();
+                    foreach(var teacher in teachersNoRepeat)
+                    {
+                        if(teacher.TeacherAvailableOrgId == org.OrgId && teacher.TeacherLevelId == level.PropValue)
+                        {
+                            teacherList.Add(new
+                            {
+                                teacher.TeacherId,
+                                TeacherName = teacher.TeacherFirstName + " " + teacher.TeacherLastName ,
+                            });
+                        }
+                    }
+
+                    levelList.Add(new
+                    {
+                        levelId = level.PropValue,
+                        levelName = level.PropName,
+                        teacher = teacherList
+                    });
+                }
+                List<Object> roomList = new List<object>();
+                foreach(var room in rooms)
+                {
+                    if(org.OrgId == room.OrgId)
+                    {
+                        roomList.Add(new
+                        {
+                            room.RoomId,
+                            room.RoomName
+                        });
+                    }
+                }
+                teacherResults.Add(new
+                {
+                    org.OrgId,
+                    org.OrgName,
+                    Room = roomList,
+                    Level = levelList
+                });
+            }
+            result.Data = teacherResults;
+
+            return Ok(result);
+        }
+    }
+}
