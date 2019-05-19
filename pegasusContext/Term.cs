0073d1ad54c92e820f1b6a959ae61e6f18365085
oliver <oliverdeng1020@gmail.com>
2019-05-20 11:03:10 +1200

    update

--- pegasusContext/Term.cs
+++ pegasusContext/Term.cs
@@ -17,11 +17,8 @@ namespace Pegasus_backend.pegasusContext
         public DateTime? EndDate { get; set; }
         public short? WeekQuantity { get; set; }
         public byte? TermType { get; set; }
-<<<<<<< HEAD
         
         [JsonIgnore]
-=======
->>>>>>> dc29658ef159cfb3115d26376761ff66c5f0de77
 
         public ICollection<Invoice> Invoice { get; set; }
         public ICollection<InvoiceWaitingConfirm> InvoiceWaitingConfirm { get; set; }
