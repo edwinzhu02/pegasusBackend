375cd5f37722d954c2ca80145355c5aad3ff11cf
lyriclau <>
2019-05-31 15:46:45 +1200

    lyric

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -444,7 +444,7 @@ namespace Pegasus_backend.Controllers
             var term = await _pegasusContext.Term.FirstOrDefaultAsync(x => x.TermId == term_id);
 
             var all_terms = await _pegasusContext.Term.Select(x => new { x.TermId, x.BeginDate, x.EndDate }).ToListAsync();
-            //int i = 0;
+            int i = 0;
             foreach (var course_instance in course_instances)
             {
                 InvoiceWaitingConfirm invoice = new InvoiceWaitingConfirm();
