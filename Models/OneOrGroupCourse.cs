fdb742904c103ee6f05343ab2fe23794645d84c3
lyriclau <>
2019-06-09 13:39:09 +1200

    lyric

---
+++ Models/OneOrGroupCourse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Pegasus_backend.pegasusContext;
+
+namespace Pegasus_backend.Models
+{
+    public class OneOrGroupCourse
+    {
+        public int isOne2one { get; set; }
+        public int CourseInstanceId { get; set; }
+        public short? RoomId { get; set; }
+        public short? TeacherId { get; set; }
+        public short? OrgId { get; set; }
+        public int GroupCourseInstanceId { get; set; }
+
+
+    }
+}
