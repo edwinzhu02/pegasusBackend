4be4ddea633250429ecc75d02a469041013a1c56
oliver <oliverdeng1005@hotmail.com>
2019-06-10 11:53:14 +1200

    update

---
+++ Controllers/RemindLogController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pegasus_backend.Models;
+using Pegasus_backend.pegasusContext;
+namespace Pegasus_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RemindLogController: BasicController
+    {
+        private readonly pegasusContext.ablemusicContext _ablemusicContext;
+
+        public RemindLogController(pegasusContext.ablemusicContext ablemusicContext)
+        {
+            _ablemusicContext = ablemusicContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRemindLog()
+        {
+            var result = new Result<Object>();
+            try
+            {
+                var item = await _ablemusicContext.RemindLog
+                    .Include(s=>s.Teacher)
+                    .Include(s=>s.Learner)
+                    .Select(s=> new
+                    {
+                        s.RemindId,s.Email,s.RemindType,s.RemindContent,s.CreatedAt,s.IsLearner,s.ProcessFlag,
+                        s.EmailAt,s.RemindTitle,s.ReceivedFlag,
+                        Learner = IsNull(s.Learner)?
+                            null:new {s.Learner.FirstName,s.Learner.LastName,s.LearnerId},
+                        Teacher= IsNull(s.Teacher)?null:new {s.Teacher.TeacherId,s.Teacher.FirstName,s.Teacher.LastName}
+                    })
+                    .ToListAsync();
+                return Ok(item);
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                result.ErrorMessage = ex.Message;
+                return BadRequest(result);
+            }
+        }
+    }
+}
