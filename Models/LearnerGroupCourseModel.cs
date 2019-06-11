c73e41711226436585ab26212efbc9203678d01b
oliver <oliverdeng1005@hotmail.com>
2019-06-11 14:01:51 +1200

    update

---
+++ Models/LearnerGroupCourseModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using Pegasus_backend.pegasusContext;
+
+namespace Pegasus_backend.Models
+{
+    public class LearnerGroupCourseModel
+    {
+        public List<LearnerGroupCourse> LearnerGroupCourses { get; set; }
+    }
+
+}
