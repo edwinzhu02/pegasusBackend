716aec11a90a779706b2f7578a3093c416335e8f
Michael Zhang <minghui0911@hotmail.com>
2019-06-24 12:25:10 +1200

    s

---
+++ Controllers/SoldTransactionController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Pegasus_backend.pegasusContext;
+using Pegasus_backend.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Pegasus_backend.Controllers;
+using Pegasus_backend.ActionFilter;
+
+namespace Pegasus_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SoldTranscationController : BasicController
+    {
+    private readonly pegasusContext.ablemusicContext _pegasusContext;
+    public SoldTranscationController(pegasusContext.ablemusicContext pegasusContext)
+        {
+            _pegasusContext = pegasusContext;
+        }
+        //GET: http://localhost:5000/api/SoldTranscation
+        [HttpGet]
+        public async Task <IActionResult> GetSold()
+        {
+            var result = new Result<Object>();
+            try 
+            {
+                result.Data = await _pegasusContext.Payment
+                .Include(s => s.SoldTransaction)
+                .ThenInclude(p => p.Product)
+                .Include(s => s.Staff)
+                .Include(s => s.Learner)
+                .Select(s => new 
+                {   s.LearnerId,
+                    Learn = (s.Learner.FirstName + s.Learner.LastName),
+                    s.StaffId,
+                    Staff = (s.Staff.FirstName + s.Staff.LastName),
+                    s.PaymentId,
+                    s.PaymentMethod,
+                    s.Amount,
+                    s.CreatedAt}).FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+           {
+                result.IsSuccess = false;
+                result.ErrorMessage = ex.Message;
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
+     }
+
+}
