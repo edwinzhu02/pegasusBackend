44b26e679996254b8e1a6023e148195456febd2f
jy202715 <jy37110@gmail.com>
2019-07-02 11:52:08 +1200

    Refacotor

---
+++ Services/NotificationEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pegasus_backend.Services
+{
+    public class NotificationEventArgs : EventArgs
+    {
+        public NotificationEventArgs(string mailTo, string mailTitle, string mailContent, int remindLogId)
+        {
+            this.mailTo = mailTo;
+            this.mailTitle = mailTitle;
+            this.mailContent = mailContent;
+            this.remindLogId = remindLogId;
+        }
+        public string mailTo { get; set; }
+        public string mailTitle { get; set; }
+        public string mailContent { get; set; }
+        public int remindLogId { get; set; }
+    }
+}
