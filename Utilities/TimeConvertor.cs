958a131f46af2efe3c480c7edb6cdbc0ea9a69b6
jy202715 <jy37110@gmail.com>
2019-07-05 16:50:42 +1200

    PeriodCourseChange refactor

--- Utilities/TimeConvertor.cs
+++ Utilities/TimeConvertor.cs
@@ -26,5 +26,20 @@ namespace Pegasus_backend.Utilities
                 return nzTime;
             }
         }
+
+        public static string getDayOfWeek(int value)
+        {
+            switch (value)
+            {
+                case 1: return "Monday";
+                case 2: return "Tuesday";
+                case 3: return "Wednesday";
+                case 4: return "Thursday";
+                case 5: return "Friday";
+                case 6: return "Saturday";
+                case 7: return "Sunday";
+                default: return ("Day of week not found");
+            }
+        }
     }
 }
