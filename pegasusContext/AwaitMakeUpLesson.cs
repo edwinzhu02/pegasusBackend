4e8b3e88e4197db45e200838c159c2c8ab13fb8f
jy202715 <jy37110@gmail.com>
2019-07-09 13:59:45 +1200

    update context

---
+++ pegasusContext/AwaitMakeUpLesson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pegasus_backend.pegasusContext
+{
+    public partial class AwaitMakeUpLesson
+    {
+        public int AwaitId { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public DateTime? SchduledAt { get; set; }
+        public DateTime? ExpiredDate { get; set; }
+        public int? MissedLessonId { get; set; }
+        public int? NewLessonId { get; set; }
+        public short? IsActive { get; set; }
+        public int? LearnerId { get; set; }
+        public int? CourseInstanceId { get; set; }
+        public int? GroupCourseInstanceId { get; set; }
+
+        public One2oneCourseInstance CourseInstance { get; set; }
+        public GroupCourseInstance GroupCourseInstance { get; set; }
+        public Learner Learner { get; set; }
+        public Lesson MissedLesson { get; set; }
+        public Lesson NewLesson { get; set; }
+    }
+}
---
+++ pegasusContext/Notices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pegasus_backend.pegasusContext
+{
+    public partial class Notices
+    {
+        public int NoticeId { get; set; }
+        public string Notice { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public int? IsCompleted { get; set; }
+        public short? FromStaffId { get; set; }
+        public short? ToStaffId { get; set; }
+
+        public Staff FromStaff { get; set; }
+        public Staff ToStaff { get; set; }
+    }
+}
