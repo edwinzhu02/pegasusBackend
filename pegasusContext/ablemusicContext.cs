3546f90ec2dc8cafad1a64d5ae6ac88846315bd6
13911383489@139.COM <13911383489@139.COM>
2019-07-15 13:40:57 +1200

    update

--- pegasusContext/ablemusicContext.cs
+++ pegasusContext/ablemusicContext.cs
@@ -78,7 +78,7 @@ namespace Pegasus_backend.pegasusContext
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseMySQL("Server=45.76.123.59;User Id=dbuser;Password=qwer1234;Database=ablemusic");
             }
         }
