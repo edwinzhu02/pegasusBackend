64e599d7384ebefe09bbfdeb1ae72b08ecf27ec9
lgxkevin <lgx9587@gmail.com>
2019-07-22 15:40:20 +1200

    chat update

--- Controllers/MobileControllers/Chatroom.cs
+++ Controllers/MobileControllers/Chatroom.cs
@@ -47,7 +47,7 @@ namespace Pegasus_backend.Controllers.MobileControllers
             var returnedMessageTime = chatMessageModel.CreateAt ?? DateTime.Now;
             try
             {
-                await Clients.User(chatMessageModel.MessageBody)
+                await Clients.User(chatMessageModel.ReceiverUserId.ToString())
                     .SendAsync("SendMessageOneToOne", chatMessageModel.SenderUserId, chatMessageModel.MessageBody,
                         returnedMessageTime.ToString("G"));
             }
@@ -58,10 +58,10 @@ namespace Pegasus_backend.Controllers.MobileControllers
             return "Message send";
         }
 
-//        public override async Task OnDisconnectedAsync(Exception exception)
-//        {
-//            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "SignalR Users");
-//            await base.OnDisconnectedAsync(exception);
-//        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnDisconnectedAsync(exception);
+        }
     }
 }
