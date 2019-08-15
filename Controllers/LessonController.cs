d731a786c8782f7e9ace1ca0fc90e6313af77378
oliver <oliverdeng1005@hotmail.com>
2019-08-15 19:38:35 +1200

    update

--- Controllers/LessonController.cs
+++ Controllers/LessonController.cs
@@ -34,8 +34,24 @@ namespace Pegasus_backend.Controllers
                 var teacherId = teacher.TeacherId;
 
                 var lessons = _ablemusicContext.Lesson
+                    .Include(s=>s.TrialCourse)
+                    .Include(s=>s.Learner)
+                    .Include(group => group.GroupCourseInstance)
+                    .ThenInclude(learnerCourse => learnerCourse.LearnerGroupCourse)
+                    .ThenInclude(learner => learner.Learner)
+                    .Include(s => s.GroupCourseInstance)
+                    .ThenInclude(w => w.Course)
+                    .Include(s => s.CourseInstance)
+                    .ThenInclude(w => w.Course)
                     .Where(s => s.BeginTime >= today.AddMonths(-1) && s.TeacherId == teacherId)
-                    .Select(s=>new {s.BeginTime,s.LessonId})
+                    .Select(s=>new
+                    {
+                        s.BeginTime,s.LessonId,
+                        CourseName=!IsNull(s.GroupCourseInstance)?s.GroupCourseInstance.Course.CourseName:IsNull(s.CourseInstance)?s.TrialCourse.CourseName:s.CourseInstance.Course.CourseName,
+                        learner = IsNull(s.GroupCourseInstance)?new List<Object>(){new {s.Learner.FirstName,s.Learner.LastName,s.Learner.LearnerId}}:null,
+                        learners = IsNull(s.GroupCourseInstance)?null:s.GroupCourseInstance.LearnerGroupCourse.
+                            Select(w=>new{w.Learner.FirstName,w.Learner.LastName,w.Learner.LearnerId}),
+                    })
                     .ToList();
                 var item = new Dictionary<string,List<object>>();
                 lessons.ForEach(s =>
@@ -51,11 +67,25 @@ namespace Pegasus_backend.Controllers
                     var time = s.BeginTime.Value.TimeOfDay;
                     if (item.ContainsKey(date))
                     {
-                        item[date].Add(new{info=new{time}});
+                        item[date].Add(new
+                        {
+                            name=s.CourseName,
+                            info=new
+                            {
+                                time,s.learner,s.learners
+                            }
+                        });
                     }
                     else
                     {
-                        item.Add(date,new List<object>{new{info=new{time}}});
+                        item.Add(date,new List<object>{new
+                        {
+                            name=s.CourseName,
+                            info=new
+                            {
+                                time,s.learner,s.learners
+                            }
+                        }});
                     }
                 });
                 
