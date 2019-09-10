b335382f3e910084c9302ec9d6e583295331577c
edwin.zhu02 <edwin.zhu02@gmail.com>
2019-09-10 22:12:34 +1200

    fix

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -78,7 +78,7 @@ namespace Pegasus_backend.Controllers
         }
 
        [HttpGet("[action]/{orgId}")]
-        public async Task<IActionResult> GetTeacherByOrg(short orgId, short dayofweek)
+        public async Task<IActionResult> GetTeacherByOrg(short orgId)
         {
             var result = new Result<object>();
             try
@@ -86,8 +86,8 @@ namespace Pegasus_backend.Controllers
                 var availableDays = await _ablemusicContext.AvailableDays
                     .Include(s=>s.Teacher)
                     .Where(s => s.OrgId == orgId && s.Teacher.IsActivate ==1 )
-                    .Select(s => new {s.OrgId,s.RoomId ,
-                            Teacher = new  {s.Teacher.FirstName,s.Teacher.LastName}
+                    .Select(s => new {s.OrgId,
+                        Teacher = new  {s.Teacher.FirstName,s.Teacher.LastName}
                         })
                     .Distinct()
                     .OrderBy(s => s.Teacher.FirstName)
