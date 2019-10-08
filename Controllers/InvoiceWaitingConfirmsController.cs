e7f16a3232a01e5d22c0ea12b9c26181fba992ff
Yinghui Zhu <edwin.zhu02@gmail.com>
2019-10-08 22:19:49 +1300

    ccc

--- Controllers/InvoiceWaitingConfirmsController.cs
+++ Controllers/InvoiceWaitingConfirmsController.cs
@@ -306,7 +306,7 @@ namespace Pegasus_backend.Controllers
                                                         IsActive = i == null ? 0 : i.IsActive,
                                                         Comment = i == null ? string.Empty : i.Comment
                                                     },
-                                                }).OrderByDescending(re => int.Parse(re.InvoiceWaitingConfirm.InvoiceNum)).ToListAsync();
+                                                }).OrderByDescending(re => re.InvoiceWaitingConfirm.InvoiceNum).ToListAsync();
             }
             catch (Exception ex)
             {
